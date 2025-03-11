54b9df68ec9c26e5878e48bd1475dbb0a6669bf3
CharlizeBouwer <ST10471059@vcconnect.edu.za>
2025-03-11 10:39:10 +0200

    new class
    We added new class

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class NewClass {
+    
+    public String strSecondClassVar ="Testing";
+    
+    public void helloclass()
+    {
+        
+        System.out.println("Hello from 2nd class");
+        
+    }
+    
+}
--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,11 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World  I just did gitHub!");
+        
+        NewClass nc = new NewClass(); // is creatio an object of newclass. nc is like variable nmae that we created 
+nc.helloclass();// have 2 use vb name 2 comunicate between classes
+
+System.out.println(nc.strSecondClassVar); // outputveriable from 2nd class
+
     }
 }
